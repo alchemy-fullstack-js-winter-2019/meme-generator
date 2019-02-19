0ea77c3e44f35b6a4e175f98a032cb346901c06e
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2019-02-19 11:40:40 -0800

    can get text in input

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,15 +1,28 @@
 import React, { PureComponent } from 'react';
 
 export default class App extends PureComponent {
+  //can update and change over time
+  state = {
+    count: 0,
+    text: ''
+  }
+  //property class
+  handleClick = () => {
+    this.setState({ count: this.state.count + 1 });
+    console.log('clicked');
+  }
+  onChange = ({ target }) => {
+    this.setState({ text: target.value });
+    
+  }
+
   render() {
     return (
       <>  
-        <h1>HI</h1>
         <button onClick={this.handleClick}> Click Here</button>
+        <input onChange={this.onChange} value={this.state.text}></input>
+        <h1>{this.state.text}</h1>
       </>
     );
-  }
-  handleClick() {
-    console.log('clicked');
   }
 }
