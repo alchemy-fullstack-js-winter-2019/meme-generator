47af5ecbe91dee87bdbaea503c822636441074c7
Paige Gorry <paigeegorry@gmail.com>
2019-02-19 11:24:59 -0800

    click counter incrementing

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import React, { PureComponent, Fragment } from 'react';
 
 class App extends PureComponent {
+  state = {
+    clickCount: 0
+  }
+
   handleClick = () => {
-    console.log('clicked!');
+    this.setState({ clickCount: this.state.clickCount + 1 });
+    console.log('clicked', this.state.clickCount, 'times');
   }
   render() {
     return (
