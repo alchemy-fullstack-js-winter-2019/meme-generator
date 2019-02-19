780b5fa62466cecf5873e9717eaf50a938253741
Dyanna Tolman <dyannast@gmail.com>
2019-02-19 11:52:44 -0800

    input box works without errors and mirrors in the page

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,28 @@ import React, { PureComponent }from 'react';
 
 class App extends PureComponent {
   state = {
-    count: 0
+    count: 0,
+    text: ''
   };
 
 handleClick = () => {
   const { count } = this.state;
   this.setState({ count: count + 1 }, () => {
-    console.log(this.state.count);
+    console.log(this.state.count, 'click/s');
   }); 
+};
+handleChange = ({ target }) => {
+  this.setState({ [target.name]: target.value });
 };
 
   render() {
+    const { text } = this.state;
     return (
       <>
       <h1>Hello</h1>
       <button onClick={this.handleClick}>click</button>
+      <input type="text" name="text" value={text} onChange={this.handleChange} />
+      <h2>{text}</h2>
       </>
     );
   }
