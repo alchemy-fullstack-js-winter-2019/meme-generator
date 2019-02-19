20efdd75cde8e56fc920faad1ec3d6d249460a75
Tyler Corbett <thetylercorbett@gmail.com>
2019-02-19 15:15:41 -0800

    refactored into components

--- src/components/App.js
+++ src/components/App.js
@@ -1,67 +1,74 @@
 import React, { PureComponent } from 'react';
-import figlet from 'figlet';
 import domToImage from 'dom-to-image';
+import figlet from 'figlet';
 import fileSaver from 'file-saver';
+import TextFormatter from './TextFormatter';
+import FormatDisplay from './FormatDisplay';
+
 
 export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.formattedTextRef = React.createRef();
+  }
   state = {
-    clickCount: 0,
+    count: 0,
     text: null,
     formatedText: '',
     font: 'Ghoulish',
     img: ''
   }
+  saveFile = () => {
+    fileSaver.saveAs(this.state.img);
+  };
+  formatText = () => {
+    const { font } = this.state;
+    figlet.text(this.state.text,
+      { font },
+      (err, formattedText) => {
+        // eslint-disable-next-line no-console
+        if(err) return console.error(err);
+
+        this.setState({ formattedText });
+      });
+  };
   handleClick = () => {
-    let clickCount = this.state.clickCount;
-    this.setState({ clickCount: clickCount + 1 }, () => {
+    const { count } = this.state;
+    this.setState({ count: count + 1 }, () => {
       // eslint-disable-next-line no-console
-      console.log(clickCount);
-      const node = document.getElementById('formatedText');
-      domToImage.toPng(node)
-        .then(dataUrl => {
-          const img = new Image();
-          img.src = dataUrl;
-          document.body.appendChild(img);
-          this.setState({ img: dataUrl });
-        })
-        .catch(err => {
-          console.error('oopies doopies, we made an errory weerrory', err);
-        });
+      console.log('Click', this.state.count);
     });
+  };
+  textToImage = event => {
+    event.preventDefault();
+    domToImage.toPng(this.formattedTextRef.current)
+      .then(img => {
+        this.setState({ img });
+      });
   };
   handleChange = ({ target }) => {
     this.setState({ [target.name]: target.value }, () => {
       this.formatText();
     });
   };
-  formatText = () => {
-    figlet.text(this.state.text, {
-      font: this.state.font
-    }, 
-    (err, formatedText) => {
-      // eslint-disable-next-line no-console
-      if(err) return console.error(err);
-      this.setState({ formatedText });
-    });
-  };
-  saveFile = () => {
-    fileSaver.saveAs(this.state.img);
-  };
   render() {
-    const fonts = ['Ghost', 'Ghoulish', 'Soft', 'Star Strips', 'Tombstone'];
-    const listOfOptions = fonts.map(f => {
-      return (
-        <option key={f} value={f}>{f}</option>
-      );
-    });
+    const { text, formattedText, font, img } = this.state;
+    
     return (
         <>
-          <button onClick={this.handleClick}>Make Picture</button>
-          <button onClick={this.saveFile}>Save Picture</button>
-          <input type="text" name="text" onChange={this.handleChange} value={this.text}></input>
-          <select name="font" value={this.font} onChange={this.handleChange}>{listOfOptions}</select>
-          <h1>{this.state.text}</h1>
-          <h2 id="formatedText"><pre>{this.state.formatedText}</pre></h2>
+          <TextFormatter
+            text={text}
+            font={font}
+            handleChange={this.handleChange}
+            textToImage={this.textToImage}
+          />
+          <FormatDisplay
+            formattedText={formattedText}
+            formattedTextRef={this.formattedTextRef}
+          />
+          {img && <img src={img}/>}
+          {img && <button onClick={this.saveFile}>Save File</button>}
+          <button onClick={this.handleClick}>Click</button>
         </>
     );
   }
