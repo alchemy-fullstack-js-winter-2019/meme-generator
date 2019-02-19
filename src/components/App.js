0b74ce5efffae6a64eba50e49fd43741bc880948
Carmen V Ramos <carmenvramos@yahoo.com>
2019-02-19 14:34:58 -0800

    Demo Create Image

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,39 @@
 import React, { PureComponent } from 'react';
 import figlet from 'figlet';
+import domToImage from 'dom-to-image';
 
 export default class App extends PureComponent {
+  constructor(props) {
+    // super calls PureComponents constructor
+    super(props);
+    this.formattedTextRef = React.createRef();
+  }
   state = {
     // hi: 'there' ==>
     count: 0, 
     text: '', 
-    formatedText: '',
-    font: 'Basic'
+    formattedText: '',
+    font: 'Basic',
+    img: ''
     
+  };
+
+  textToImage = event => {
+    event.preventDefault();
+    domToImage.toPng(this.formattedTextRef.current)
+    .then(img => {
+      this.setState({ img })
+    });
   };
 
   formatText = () => {
     const { font } = this.state;
     figlet.text(this.state.text,
       { font },
-      (err, formatedText) => {
+      (err, formattedText) => {
         if(err) return console.error(err);
 
-        this.setState({ formatedText });
+        this.setState({ formattedText });
       });
   };
 
@@ -39,19 +54,23 @@ export default class App extends PureComponent {
   };
 
   render() {
-    const { text, formatedText, font } = this.state;
+    const { text, formattedText, font, img } = this.state;
     const fontOptions = ['Ghost', 'Weird', 'Chunky', 'Basic', 'Lil Devil'].map(f => {
       return <option key={f} value={f}>{f}</option>
     });
 
     return (
       <>
+      <form onSubmit={this.textToImage}>
       <select name="font" onChange={this.handleChange} value={font}> 
       {fontOptions}
       </select>
       <input type="text" name="text" value={text} onChange={this.handleChange} />
+      <button type="submit">Create Image</button>
+      </form>
       <h1>{text}</h1>
-      <h2><pre>{formatedText}</pre></h2>
+      <h2 ref={this.formattedTextRef}><pre>{formattedText}</pre></h2>
+        {img && <img src={img} />}
       <button onClick={this.handleClick} >
         Click 
       </button>
