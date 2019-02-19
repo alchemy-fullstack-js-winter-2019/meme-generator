388ec011a5881947ede32faab84f72c2d1f74237
Tyler Corbett <thetylercorbett@gmail.com>
2019-02-19 11:40:29 -0800

    can console log text as it changes in text box

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,26 @@ import React, { PureComponent } from 'react';
 
 export default class App extends PureComponent {
   state = {
-    clickCount: 0
+    clickCount: 0,
+    text: null
   }
   handleClick = () => {
     let clickCount = this.state.clickCount;
     this.setState({ clickCount: clickCount + 1 }, () => {
+      // eslint-disable-next-line no-console
       console.log(this.state.clickCount);
     });
+  };
+  handleChange = ({ target }) => {
+    console.log(target.value);
+   
   };
   render() {
     return (
         <>
           <h1>Hi</h1>
           <button onClick={this.handleClick}>Press me</button>
+          <input id="text" type="text" onChange={this.handleChange}></input>
         </>
     );
   }
