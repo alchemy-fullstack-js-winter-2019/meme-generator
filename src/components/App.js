ab2bc24d086a2b31c26fd296c754d701feeb6456
Kristin Hortsch <kristinhortsch@gmail.com>
2019-02-19 11:19:51 -0800

    click counter working

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
+/*eslint-disable no-console*/
 import React, { Fragment, PureComponent } from 'react';
 import 'normalize-css';
 
 //class components are used when we need to store state
 export default class App extends PureComponent {
+  state = {
+    clicks: 0
+  }
+
   handleClick = () => {
+    this.setState({ clicks: this.state.clicks + 1 });
+    console.log(this.state.clicks);
     console.log('clicked');
   }
 
   render() {
     return (
       <Fragment>
-        <h1>Hello TEONNA</h1>
+        <h1>Hello Teonna</h1>
         <button onClick={this.handleClick}>Click Me</button>
       </Fragment>
     );
