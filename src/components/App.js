336c6b9b30aeb1e7acad86eb370760402fbb94e3
Paige Gorry <paigeegorry@gmail.com>
2019-02-19 12:27:17 -0800

    figlet working

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
 import React, { PureComponent, Fragment } from 'react';
+import figlet from 'figlet';
 
 class App extends PureComponent {
   state = {
     clickCount: 0,
-    text: 'test text'
+    text: '',
+    formattedText: ''
+  }
+
+  formatText = () => {
+    figlet.text(this.state.text, {
+      font: 'Ghoulish',
+      horizontalLayout: 'default',
+      verticalLayout: 'default' }, (err, data) => {
+      if(err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+      }
+      console.log(data);
+      this.setState({ formattedText: data });
+    }); 
   }
 
   handleClick = () => {
@@ -12,17 +29,21 @@ class App extends PureComponent {
     });
   }
 
-  handleText = (e) => {
-    this.setState({ text: e.target.value });
+  handleText = ({ target }) => {
+    this.setState({ text: target.value }, () => {
+      this.formatText();
+    });
   }
 
   render() {
+    const { text, formattedText } = this.state;
     return (
       <Fragment>
         <h1>Heyo</h1>
-        <input type="text" onChange={this.handleText}></input>
+        <input type="text" name="text" value={text} onChange={this.handleText}></input>
         <button onClick={this.handleClick}>Button</button>
-        <h1>{this.state.text}</h1>
+        <h1>{formattedText}</h1>
+        <p>{text}</p>
       </Fragment>
     );
   }
