646644de5c177bb94567532047b231d3b67d26b3
Shaba9 <shaba92@gmail.com>
2019-02-19 11:26:07 -0800

    count added to state

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
 import React, { PureComponent } from 'react';
 
 export default class App extends PureComponent {
+  state = {
+    hi: 'there',
+    count: 0
+  }
+
+  handleClick = () => {
+    this.setState({ hi: 'you', count: this.state.count + 1 });
+    console.log(this.state.hi, this.state.count);
+    console.log('yo');
+  }
+
   render() {
     return (
-      <h1>Hi</h1>
+      <>
+        <h1>Hi</h1>
+        <button onClick={this.handleClick}>Click Me!</button>
+      </>
     );
   }
 }
