98006e13b1a6430b1d2271eb982761f33f7b5990
Kristin Hortsch <kristinhortsch@gmail.com>
2019-02-19 12:22:29 -0800

    formatted font is showing up!!!!!

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 /*eslint-disable no-console*/
 import React, { Fragment, PureComponent } from 'react';
 import 'normalize-css';
+import figlet from 'figlet';
 
 //class components are used when we need to store state
 export default class App extends PureComponent {
   state = {
-    clicks: 0
+    clicks: 0,
+    text: '',
+    formattedText: ''
   }
 
   handleClick = () => {
@@ -14,11 +17,47 @@ export default class App extends PureComponent {
     console.log('clicked');
   }
 
+  handleChange = (e) => {
+    this.setState({ text: e.target.value });
+    this.formatText();
+  }
+
+  formatText = () => {
+    // this.setState({ formattedText: figlet.text(this.state.text, {
+    //   font: 'Ghost',
+    //   horizontalLayout: 'default',
+    //   verticalLayout: 'default'
+    // }, function(err, data) {
+    //   if(err) {
+    //     console.log('Something went wrong...');
+    //     console.dir(err);
+    //     return;
+    //   }
+    //   console.log(data);
+    // }) });
+
+    figlet.text(this.state.text, {
+      font: 'Ghost',
+      horizontalLayout: 'default',
+      verticalLayout: 'default'
+    }, (err, data) => {
+      if(err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+      }
+      console.log('data', data);
+      this.setState({ formattedText: data });
+      
+    });
+  }
+
   render() {
     return (
       <Fragment>
-        <h1>Hello Teonna</h1>
+        <input type="text" value={this.state.text} onChange={this.handleChange}></input>
         <button onClick={this.handleClick}>Click Me</button>
+        <h1>{this.state.formattedText}</h1>
       </Fragment>
     );
   }
