2faabfe24a9d8e6fd239d171d983a8e3099d1373
Paige Gorry <paigeegorry@gmail.com>
2019-02-19 11:29:19 -0800

    added callback to handleClick to get the correct number of clicks

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,24 @@ import React, { PureComponent, Fragment } from 'react';
 
 class App extends PureComponent {
   state = {
-    clickCount: 0
+    clickCount: 0,
+    text: 'test text'
   }
 
   handleClick = () => {
-    this.setState({ clickCount: this.state.clickCount + 1 });
-    console.log('clicked', this.state.clickCount, 'times');
+    this.setState({ clickCount: this.state.clickCount + 1 }, () => {
+      console.log('clicked', this.state.clickCount, 'times');
+    });
   }
+
+
   render() {
     return (
       <Fragment>
         <h1>Heyo</h1>
+        <input type="text" onChange={this.setState}></input>
         <button onClick={this.handleClick}>Button</button>
+        <h1>{this.state.text}</h1>
       </Fragment>
     );
   }
