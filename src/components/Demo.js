7584f339cd4c7cf3921be0c6d619758d56e78322
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2019-02-19 15:47:50 -0800

    can kinda see figlet on browser

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,28 +1,45 @@
 import React, { PureComponent } from 'react';
+import figlet from 'figlet';
 
 export default class App extends PureComponent {
   //can update and change over time
   state = {
     count: 0,
-    text: ''
+    text: '',
+    formatedText: ''
   }
   //property class
   handleClick = () => {
     this.setState({ count: this.state.count + 1 });
     console.log('clicked');
   }
+
   onChange = ({ target }) => {
     //target.name refers to name in input can have multiple 
-    this.setState({ [target.name]: target.value });
-    
+    this.setState({ [target.name]: target.value }, () => {
+      this.formatText();
+    }); 
   }
 
+  formatText = () => {
+    const font = 'Greek';
+    figlet.text(this.state.text, 
+      { font },
+      (err, formatedText) => {
+        if(err) return console.log(err);
+        this.setState({ formatedText });
+      });
+  };
+
   render() {
+    const { text, formatedText } = this.state;
     return (
+
       <>  
         <button onClick={this.handleClick}> Click Here</button>
-        <input type = "text"  name="text" onChange={this.onChange} value={this.state.text}></input>
-        <h1>{this.state.text}</h1>
+        <input type="text"  name="text" onChange={this.onChange} value={this.state.text}></input>
+        <h1>{text}</h1>
+        <h2>{formatedText}</h2>
       </>
     );
   }
