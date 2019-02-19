70154b6cdfdb6e338bfa1e8b3a817d56d72bbaa2
Sophie Graham <sophiergraham1@gmail.com>
2019-02-19 14:46:43 -0800

    added save to file, got buttons to work

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,32 @@
 import React, { PureComponent } from 'react';
 import figlet from 'figlet';
+import DomToImage from 'dom-to-image';
+import fileSaver from 'fileSaver';
 
 export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.formattedTextRef = React.createRef();
+  }
+
   state = {
     count: 0,
     text: '',
     formatedText: '',
-    font: 'Basic'
+    font: 'Basic',
+    img:''
+  };
+
+  saveFile = () => {
+    fileSaver.saveAs(this.state.img);
+  };
+
+  textToImage = event => {
+    event.preventDefault();
+    DomToImage.toPng(this.formattedTextRef.current)
+      .then(img => {
+        this.setState({ img });
+      });
   };
 
   formatText = () => {
@@ -40,19 +60,25 @@ export default class App extends PureComponent {
   };
 
   render() {
-    const { text, formatedText, font } = this.state;
+    const { text, formatedText, font, img } = this.state;
 
-    const fontOptions = ['Ghost',  'Weird', 'Chunky', 'Basic', 'Lil Devil'].map(f => {
-      return <option key={f}  value={f}>{f}</option>;
+    const fontOptions = ['Ghost', 'Weird', 'Chunky', 'Basic', 'Lil Devil'].map(f => {
+      return <option key={f} value={f}>{f}</option>;
     });
 
     return (
       <>
-        <select name="font" onChange={this.handleChange} value={font}>
-          {fontOptions}
-        </select>
-        <input type="text" name="text" value={text} onChange={this.handleChange}/>
-        <h2><pre>{formatedText}</pre></h2>
+        <form onSubmit={this.textToImage}>
+          <select name="font" onChange={this.handleChange} value={font}>
+            {fontOptions}
+          </select>
+          <input type="text" name="text" value={text} onChange={this.handleChange}/>
+          <button type="submit">Create Image</button>
+        </form>
+
+        <h2 ref={this.formattedTextRef}><pre>{formatedText}</pre></h2>
+        {img && <img src= {img}/>}
+        {img && <button onClick={this.saveFile}>SaveFile</button>}
         <button onClick={this.handleClick}>Click</button>
       </>
     );
