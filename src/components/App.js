411dc622a8b0ebd9a5c69d6c4b528d39f73130cb
Tyler Corbett <thetylercorbett@gmail.com>
2019-02-19 10:49:31 -0800

    app rendering in browser

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import React, { PureComponent } from 'react';
 class App extends PureComponent {
   render() {
     return (
-      <h1>Hi</h1>
+        <>
+          <h1>Hi</h1>
+          <button>Press me</button>
+        </>
     );
   }
 }
