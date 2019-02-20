18a42288664c16ab4a90dd0df76597e243c499e4
Kate Dameron <katerj33@gmail.com>
2019-02-19 16:17:12 -0800

    set up App with first input and change handler

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React, { PureComponent } from 'react';
+
+export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.formattedTextRef = React.createRef();
+  }
+
+  state = {
+    headerText: 'Add Header Text',
+    footerText: '',
+    imgUrl: ''
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+    console.log(target.value);
+  }
+
+  render() {
+    const { headerText, footerText, imgUrl } = this.state;
+    return (
+      <>
+      <h1>Meme Generatoooor</h1>
+      <input onChange={this.handleChange} type="text" name="headerText" value={headerText}></input>
+      </>
+    );
+  }
+}
