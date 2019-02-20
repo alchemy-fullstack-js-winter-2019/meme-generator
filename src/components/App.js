ba7bbf4deae6fe2fbfc88d2e83cb2723ec176bd9
Shaba9 <shaba92@gmail.com>
2019-02-19 20:33:43 -0800

    text positioned on top of image

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import fileSaver from 'file-saver';
 import domToImg from 'dom-to-image';
 import 'normalize.css';
-import './App.css';
+import styles from './App.css';
 
 export default class App extends PureComponent {
   state = {
@@ -33,10 +33,10 @@ export default class App extends PureComponent {
         <p>Image URL: <input name="img" type="text" value={img} onChange={this.handleChange}/></p>
         <p>Header: <input name="header" type="text" value={header} onChange={this.handleChange}/></p>
         <p>Footer: <input name="footer" type="text" value={footer} onChange={this.handleChange}/></p>
-        <div id="meme">
-          <h2>{header}</h2>
+        <div id="meme" className={styles.meme}>
+          <h2 className={styles.header}>{header}</h2>
           <img src={img} />
-          <h2>{footer}</h2>
+          <h2 className={styles.footer}>{footer}</h2>
         </div>
         <button onClick={this.saveImage}>Save Image</button>
       </>
