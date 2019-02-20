fa49545d6a873d8a60a6e54c2cbfb0bc1b34bc18
Kate Dameron <katerj33@gmail.com>
2019-02-19 21:50:33 -0800

    updated app with some extra styling and added some logic to try to upload image from a computer but only have an alert with file name so far

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,14 @@ export default class App extends PureComponent {
   constructor(props) {
     super(props);
     this.memeTextRef = React.createRef();
+    this.fileInput = React.createRef();
   }
 
   state = {
-    headerText: 'Add Header Text',
-    footerText: 'Add Footer Text',
+    headerText: '',
+    footerText: '',
     imgUpload: '',
-    imgUrl: 'https://static01.nyt.com/images/2017/09/12/us/12xp-monkey1/12xp-monkey1-articleInline.jpg?quality=75&auto=webp&disable=upscale',
+    imgUrl: '',
     meme: ''
   }
 
@@ -34,17 +35,37 @@ export default class App extends PureComponent {
     fileSaver.saveAs(this.state.meme);
   }
 
+  handleFileUpload = event => {
+    event.preventDefault();
+    alert(`file uploaded: ${this.fileInput.current.files[0].name}`);
+  }
+
   render() {
-    const { headerText, footerText, imgUrl } = this.state;
+    const { headerText, footerText, imgUrl, imgUpload } = this.state;
     return (
       <>
       <h1>Meme Generatoooor</h1>
       <form className={styles.form} onSubmit={this.divToImage}>
-        <input onChange={this.handleChange} type="text" name="headerText" value={headerText}></input>
-        <input onChange={this.handleChange} type="text" name="footerText" value={footerText}></input>
-        <input onChange={this.handleChange} type="text" name="imgUrl" value={imgUrl}></input>
-        {/* <h2>OR</h2>
-      <input onChange={this.handleChange} type="file" name="imgUpload" value={imgUpload}></input> */}
+
+        <label> Add Header Text
+          <input onChange={this.handleChange} type="text" name="headerText" value={headerText}></input>
+        </label>
+
+        <label> Add Footer Text
+          <input onChange={this.handleChange} type="text" name="footerText" value={footerText}></input>
+        </label>
+
+        <label> Import An Image From The Internets
+          <input onChange={this.handleChange} type="text" name="imgUrl" value={imgUrl}></input>
+        </label>
+
+        <h2>OR</h2>
+
+        <label> Upload An Image From Your Computer
+          <input onChange={this.handleChange} type="file" name="imgUpload" ref={this.fileInput} value={imgUpload}></input>
+          <button type="submit" onClick={this.handleFileUpload} value={imgUpload}>Upload Image</button>
+        </label>
+        
         <button type="submit" onClick={this.saveMeme}>Save My Meme!</button>
       </form>
      
