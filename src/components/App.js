2a2bc39f2d3bf63ae6ef8f43c1fc8f7b2985483a
Paige Gorry <paigeegorry@gmail.com>
2019-02-20 11:49:30 -0800

    moved meme into its own component

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import styles from '../css/App.css';
 import domToImage from 'dom-to-image';
 import fileSaver from 'file-saver';
 import MemeForm from './MemeForm.js';
-// import Meme from './MemeForm.js';
+import Meme from './Meme.js';
 
 class App extends Component {
   constructor(props) {
@@ -35,26 +35,27 @@ class App extends Component {
       <Fragment>
         <div className={styles.meme}>
           <h1>mEmE GeNeRaToR</h1>
+          
           <MemeForm 
-            header={this.header}
-            footer={this.footer}
-            url={this.url}
+            header={header}
+            footer={footer}
+            url={url}
             handleChange={this.handleChange}/>
+          <button onClick={this.createAndSaveMeme}>Save Meme</button>
         
-          <div ref={this.finalMeme} className={styles.final}>
+          <Meme 
+            memeRef={this.finalMeme}
+            header={header}
+            footer={footer}
+            url={url} />
+
+          {/* <div ref={this.finalMeme} className={styles.final}>
             {header &&  <p>{header}</p>}
             <img src={url} />
             {footer && <p>{footer}</p>}
-          </div>
+          </div> */}
 
-          {url && header && <button onClick={this.createAndSaveMeme}>Save Meme</button>}
 
-          {/* <Meme 
-            ref={this.finalMeme}
-            header={this.header}
-            footer={this.footer}
-            url={this.url}
-            createAndSaveMeme={this.createAndSaveMeme}/> */}
         </div>
       </Fragment>
     );
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,33 +1,27 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styles from '../css/App.css';
 
 function Meme({
-  ref,
+  memeRef,
   header,
   footer,
-  url,
-  createAndSaveMeme
+  url
 }) {
   return (
-    <Fragment>
-      <div ref={ref} className={styles.final}>
-        {header &&  <p>{header}</p>}
-        <img src={url} />
-        {footer && <p>{footer}</p>}
-      </div>
-
-      {url && header && <button onClick={createAndSaveMeme}>Save Meme</button>}
-    </Fragment>
+    <div ref={memeRef} className={styles.final}>
+      {header &&  <p>{header}</p>}
+      {url && <img src={url} />}
+      {footer && <p>{footer}</p>}
+    </div>
   );
 }
 
 Meme.propTypes = {
-  ref: PropTypes.object,
+  memeRef: PropTypes.object,
   header: PropTypes.string,
   footer: PropTypes.string,
-  url: PropTypes.string,
-  createAndSaveMeme: PropTypes.func.isRequired
+  url: PropTypes.string
 };
 
 export default Meme;
