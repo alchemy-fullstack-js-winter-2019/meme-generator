47d621d8e79807b6bdb61d553a911ef698871018
Dyanna Tolman <dyannast@gmail.com>
2019-02-19 22:24:12 -0800

    after multiple attempts url input  and header input shows in dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { PureComponent }from 'react';
-import Header from './Header';
 import domToImage from 'dom-to-image';
-// import FileSaver from './FileSaver';
+import FileSaver from './FileSaver';
 
 class App extends PureComponent {
   constructor(props) {
@@ -11,14 +10,14 @@ class App extends PureComponent {
   }
 
   state = {
-    text:'',
+    text: '',
     formattedText: '',
     img: ''
   };
 
 textToImage = event => {
   event.preventDefault();
-  domToImage.toPng(this.formattedTextRef  .current)
+  domToImage.toPng(this.formattedTextRef.current)
   .then(img => {
     this.setState({ img });
   });
@@ -28,20 +27,30 @@ handleChange = ({ target }) => {
   this.setState({ [target.name]: target.value },() => {
   });
 };
+
+// uploadFile = ({ target }) => {
+//   const imgUpload = URL.createObjectURL(target.files[0]);
+//   console.log(imgUpload);
+//   this.setState({ imgUpload });
+// };
   
 render() {  
     const { text, formattedText, img } = this.state;
     return (
       <>
       <form onSubmit= {this.textToImage}>
-      <Header />
+      {/* <input type="file" onChange={this.uploadFile} />
+      <img src={this.state.imgUpload} /> */}
       <input type="text" name="text" value={text} onChange={this.handleChange} />
-      <button onClick={this.handleClick}>Save Url</button>
+      <h1>{text}</h1>
       <input type="text" name="img" value={img} onChange={this.handleChange} />
-      <button onClick={this.handleClick}>Save Url</button>
       </form>
+      <div>
       <h2 ref={this.formattedTextRef}><pre>{formattedText}</pre></h2>
-      {/* {img && <img src={img} />} */}
+      {img && <img src={text} />}
+      {img && <FileSaver img={img} />}
+      <button onClick={this.handleClick}>Save</button>
+      </div>
       </>
     )
   }
