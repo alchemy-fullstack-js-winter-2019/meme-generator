adc36fcf6ff2d993c7fa709669fbc2797ebb3fa9
Paige Gorry <paigeegorry@gmail.com>
2019-02-19 16:47:54 -0800

    i guess i didnt need a button after all.

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,34 @@
 import React, { PureComponent, Fragment } from 'react';
+import styles from '../css/App.css';
 
 class App extends PureComponent {
   state = {
     header: '',
     footer: '',
-    url: '',
-    clicked: false
+    url: ''
   }
 
   handleChange = ({ target }) => {
     this.setState({ [target.name]: target.value });
   }
 
-  handleClick = event => {
-    event.preventDefault();
-    this.setState({ clicked: true }, () => {
-      console.log(this.state);
-    });
-  }
-
   render() {
-    const { header, footer, url, clicked } = this.state;
+    const { header, footer, url } = this.state;
     return (
       <Fragment>
-        <h1>Meme Generator</h1>
-        <form>
-          <input type="text" name="header" value={this.header} onChange={this.handleChange}></input>
-          <input type="text" name="footer" value={this.footer} onChange={this.handleChange}></input>
-          <input type="url" name="url" value={this.url} onChange={this.handleChange}></input>
-          <button value={this.clicked} onClick={this.handleClick}>Create Meme</button>
-        </form>
-        {clicked && <p>{header}</p>}
-        {clicked && <img src={url} />}
-        {clicked && <p>{footer}</p>}
+        <div className={styles.meme}>
+          <h1>Meme Generator</h1>
+          <form>
+            <input type="text" name="header" value={this.header} onChange={this.handleChange}></input>
+            <input type="text" name="footer" value={this.footer} onChange={this.handleChange}></input>
+            <input type="url" name="url" value={this.url} onChange={this.handleChange}></input>
+          </form>
+        </div>
+        <div className={styles.final}>
+          <p>{header}</p>
+          <img src={url} />
+          <p>{footer}</p>
+        </div>
       </Fragment>
     );
   }
