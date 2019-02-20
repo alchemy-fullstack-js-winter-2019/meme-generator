947575da9d76e1cbc51bcfec82f091043b952cf5
Paige Gorry <paigeegorry@gmail.com>
2019-02-19 16:00:38 -0800

    header is updating based on input

--- src/components/App.js
+++ src/components/App.js
@@ -1,79 +1,28 @@
 import React, { PureComponent, Fragment } from 'react';
-import figlet from 'figlet';
-import domToImage from 'dom-to-image';
-import fileSaver from 'file-saver';
 
 class App extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.formattedTextRef = React.createRef();
-  }
-
   state = {
-    clickCount: 0,
-    text: '',
-    formattedText: '',
-    font: 'Ghoulish',
-    img: ''
-  }
-
-  formatText = () => {
-    const { font } = this.state;
-    figlet.text(this.state.text, {
-      font }, (err, data) => {
-      if(err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-      }
-      console.log(data);
-      this.setState({ formattedText: data });
-    }); 
-  }
-
-  saveFile = () => {
-    fileSaver.saveAs(this.state.img);
-  }
-
-  textToImage = event => {
-    event.preventDefault();
-    domToImage.toPng(this.formattedTextRef.current)
-      .then(img => {
-        this.setState({ img }); 
-      });
-  }
-
-  handleClick = () => {
-    this.setState({ clickCount: this.state.clickCount + 1 }, () => {
-      console.log('clicked', this.state.clickCount, 'times');
-    });
+    header: '',
+    footer: '',
+    url: ''
   }
 
   handleChange = ({ target }) => {
     this.setState({ [target.name]: target.value }, () => {
-      this.formatText();
+      console.log(this.state.header);
     });
   }
 
   render() {
-    const { text, formattedText, img } = this.state;
-
-    const fontOptions = ['Stellar', 'Speed', 'Ghost', 'Ghoulish'].map(f => {
-      return <option key={f} value={f}>{f}</option>;
-    });
     return (
       <Fragment>
-        <h1>Figlet Generator!</h1>
-        <form className="form" onSubmit={this.textToImage}>
-          <input type="text" name="text" value={text} onChange={this.handleChange}></input>
-          <select name="font" onChange={this.handleChange}>
-            {fontOptions}
-          </select>
-          <button>Submit</button>
+        <h1>Meme Generator</h1>
+        <form>
+          <input type="text" name="header" value={this.header} onChange={this.handleChange}></input>
+          <input type="text" value="footer"></input>
+          <input type="url" value="url"></input>
+          <button>Create Meme</button>
         </form>
-        <h2 ref={this.formattedTextRef}><pre>{formattedText}</pre></h2>
-        <p>{text}</p>
-        {img && <p><img src={img} /> <button onClick={this.saveFile}>Save File!</button></p>}
       </Fragment>
     );
   }
