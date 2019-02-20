61c44d0664f15a38126de4769a174bb965dbbd0b
Kristin Hortsch <kristinhortsch@gmail.com>
2019-02-20 09:28:10 -0800

    fixed the margin situation so the pic saves correctly now

--- src/components/ImageFormatter.js
+++ src/components/ImageFormatter.js
@@ -6,10 +6,12 @@ function ImageFormatter({ header, footer, imageUrl }) {
   return (
     <Fragment>
       <img className={styles.background} src='https://visme.co/blog/wp-content/uploads/2017/07/50-Beautiful-and-Minimalist-Presentation-Backgrounds-037.jpg'/>
-      <div className={styles.meme} id="meme">
-        <p className={styles.header}>{header}</p>
-        <img src={imageUrl} className={styles.img}/>
-        <p className={styles.footer}>{footer}</p>
+      <div className={styles.container}>
+        <div className={styles.meme} id="meme">
+          <p className={styles.header}>{header}</p>
+          <img src={imageUrl} className={styles.img}/>
+          <p className={styles.footer}>{footer}</p>
+        </div>
       </div>
     </Fragment>
   );
