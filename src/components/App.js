88469a854fe8b5689c2029285322a76a46ad424a
Paige Gorry <paigeegorry@gmail.com>
2019-02-19 17:26:47 -0800

    image saving working now that I have http images

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,36 @@
 import React, { PureComponent, Fragment } from 'react';
 import styles from '../css/App.css';
+import domToImage from 'dom-to-image';
+import fileSaver from 'file-saver';
 
 class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.finalMeme = React.createRef();
+  }
+
   state = {
     header: '',
     footer: '',
-    url: ''
+    url: '',
+    img: ''
   }
 
   handleChange = ({ target }) => {
     this.setState({ [target.name]: target.value });
+  }
+
+  memeToImage = event => {
+    event.preventDefault();
+    domToImage.toPng(this.finalMeme.current)
+      .then(img => {
+        this.setState({ img });
+      });
+  }
+
+  saveMeme = event => {
+    this.memeToImage(event);
+    fileSaver.saveAs(this.state.img);
   }
 
   render() {
@@ -19,15 +40,25 @@ class App extends PureComponent {
         <div className={styles.meme}>
           <h1>Meme Generator</h1>
           <form>
-            <input type="text" name="header" value={this.header} onChange={this.handleChange}></input>
-            <input type="text" name="footer" value={this.footer} onChange={this.handleChange}></input>
-            <input type="url" name="url" value={this.url} onChange={this.handleChange}></input>
+            <label>
+              Header:
+              <input type="text" name="header" value={this.header} onChange={this.handleChange}></input>
+            </label> 
+            <label>
+              Footer:
+              <input type="text" name="footer" value={this.footer} onChange={this.handleChange}></input>
+            </label>
+            <label>
+              Photo Url:
+              <input type="url" name="url" value={this.url} onChange={this.handleChange}></input>
+            </label>
           </form>
         </div>
-        <div className={styles.final}>
+        <div ref={this.finalMeme} className={styles.final}>
           <p>{header}</p>
           <img src={url} />
           <p>{footer}</p>
+          {url && <button onClick={this.saveMeme}>Save Meme</button>}
         </div>
       </Fragment>
     );
