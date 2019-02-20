523bcf90cf8495bed3ffec4ceb392feea9091f09
JeiWolfei <jvampy22@gmail.com>
2019-02-19 20:39:18 -0800

    format display added

--- src/components/FormatDisplay.js
+++ src/components/FormatDisplay.js
@@ -2,12 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function FormatDisplay({ formattedText }) {
-  return <h2 ref ={formattedTextRef}<pre></h2>
+  return <h2 ref={formattedTextRef}><pre>{formattedText}</pre></h2>;
 }
 
 FormatDisplay.propTypes = {
-  formattedText: PropTypes.string
-}
-saveFile = () => {
-  fileSaver.saveAs(this.state.img)
-}
+  formattedText: PropTypes.string,
+};
+
+export default FormatDisplay;
