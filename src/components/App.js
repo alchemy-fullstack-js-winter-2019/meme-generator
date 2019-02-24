4c68622ed2ce4641b72438e067a64eaa87d1971f
Carmen Perezchica <crperezchica@gmail.com>
2019-02-24 13:56:18 -0800

    removed duplication of file save image code

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import domToImage from 'dom-to-image';
 import DisplayMeme from '../components/DisplayMeme';
 import fileSaver from 'file-saver';
 
-class App extends PureComponent { 
+class App extends Component { 
   constructor(props) {
     super(props);
     this.memeRef = React.createRef();
@@ -15,24 +15,20 @@ class App extends PureComponent {
   };
 
 
-  saveFile = () => {
-    fileSaver.savesAs(this.state.imgUrl);
-  };
-
   //creates an image and saves to computer
-  image = event => { 
+  image = async(event) => { 
     event.preventDefault();
-    //const img = await domToImage.toPng(this.memeRef.current)
-    domToImage.toPng(this.memeRef.current)
-      .then(img => {
-        fileSaver.saveAs(img);
-        // this.setState({ img });
-      });
+    const img = await domToImage.toPng(this.memeRef.current);
+    fileSaver.saveAs(img);
+    //example of another way to write code
+    // domToImage.toPng(this.memeRef.current)
+    //   .then(img => {
+    //     fileSaver.saveAs(img); //this.setState({ img });
+    // });
   };
 
   handleChange = ({ target }) => {
-    this.setState({ [target.name]: target.value }, () => {
-    });
+    this.setState({ [target.name]: target.value });
   };
 
   render() {
@@ -43,7 +39,7 @@ class App extends PureComponent {
       <form onSubmit={this.image}>
         <input type="text" name="header" value={this.state.header} onChange={this.handleChange}/>
         <input type="text" name="footer" value={this.state.footer} onChange={this.handleChange} />
-        <input type="text" name="imgUrl" value={this.state.imgUrl} onChange={this.handleChange} />
+        <input type="url" name="imgUrl" value={imgUrl} onChange={this.handleChange} />
         <button type="submit" name="image">Save Meme</button>
       </form>
       <DisplayMeme
