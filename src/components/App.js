a37575a985e837a0b511cf890f6b661663478c82
Carmen V Ramos <carmenvramos@yahoo.com>
2019-02-27 08:39:11 -0800

    Save image func now works!

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,24 @@
 import React, { PureComponent } from 'react';
+import fileSaver from 'file-saver';
 import domToImage from 'dom-to-image';
-import FileSaver from './FileSaver';
-
 
 export default class App extends PureComponent {
   
   state = {
     header: '', 
     footer: '', 
-    img: 'https://www.castlighthealth.com/wp-content/uploads/2018/03/HealthyFood.jpg'
+    img: ''
+    // img: 'https://www.castlighthealth.com/wp-content/uploads/2018/03/HealthyFood.jpg'
   };
 
-  fileSave = () => {
-    FileSaver.saveAs(this.state.img);
-  }
-
-  // toImage = event => {
-  //   event.preventDefault();
-  //   const meme = document.getElementById('meme');
-  //   domToImage.toPng(meme)
-  //     .then(img => {
-  //       this.setState({ img });
-  //     });
-  // };
-
-  toImage = event => {
+   saveImage = event => {
     event.preventDefault();
-    const node = document.getElementById('meme');
-    domToImg.toPng(node)
-      .then(img => this.setState({ img }, () => {
-        fileSaver.saveAs(this.state.img);
-      }));
-
-   }
-
-  
+    const meme = document.getElementById('meme');
+    domToImage.toPng(meme)
+      .then(img => {
+        fileSaver.saveAs(img);
+      });
+   }  
 
   handleChange = ({ target }) => {
     this.setState({ [target.name]: target.value });
@@ -45,15 +29,16 @@ export default class App extends PureComponent {
 
     return (
       <>
-        <p>Header: <input name="header" type="text" value={header} onChange={this.handleChange}/></p>
-        <p>Image URL: <input name="img" type="text" value={img} onChange={this.handleChange}/></p>
-        <p>Footer: <input name="footer" type="text" value={footer} onChange={this.handleChange}/></p>
+        <h1>Meme Generator</h1>
+        <span>  Header: <input name="header" type="text" value={header} onChange={this.handleChange}/></span>
+        <span>  Image URL: <input name="img" type="text" value={img} onChange={this.handleChange}/></span>
+        <span>  Footer: <input name="footer" type="text" value={footer} onChange={this.handleChange}/></span>
         <div id="meme">
           <h2><pre>{header}</pre></h2>
-          <img src={img} />
+          {img && <img src={img} />}
           <h2>{footer}</h2>
         </div>
-        <button onClick={this.toImage}>Save Image</button>
+        {img &&<button onClick={this.saveImage}>Save Image</button>}
       </>
     );
   }
